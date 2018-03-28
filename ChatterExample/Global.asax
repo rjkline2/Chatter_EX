f95d8618eb17fa65ce2c6affe885f5ff5f5f4daa
RJ Kline <rj.kline@gmail.com>
2018-03-28 10:13:45 -0400

    Initial Commit

---
+++ ChatterExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatterExample.MvcApplication" Language="C#" %>
---
+++ ChatterExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterExample.Startup))]
+namespace ChatterExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
